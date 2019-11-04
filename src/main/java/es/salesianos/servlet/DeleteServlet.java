a1151c9fee7c77bf57d03e4d811038a339ab3afa
JoTerrance <gorka.zgz@gmail.com>
2019-11-04 13:25:06 +0100

    now it deletes name and surname from database

--- src/main/java/es/salesianos/servlet/DeleteServlet.java
+++ src/main/java/es/salesianos/servlet/DeleteServlet.java
@@ -1,6 +1,10 @@
 package es.salesianos.servlet;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +12,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class DeleteServlet extends HttpServlet {
+	private static final String jdbcUrl = "jdbc:h2:file:./src/main/resources/test;INIT=RUNSCRIPT FROM 'classpath:scripts/create.sql'";
+
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -15,6 +21,41 @@ public class DeleteServlet extends HttpServlet {
 		String name = req.getParameter("name");
 		String surname = req.getParameter("surname");
 		System.out.println(name + " " + surname);
+		Connection conn;
+		try {
+			Class.forName("org.h2.Driver");
+			conn = DriverManager.getConnection(jdbcUrl, "sa", "");
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		PreparedStatement preparedStatement = null;
+		try {
+			preparedStatement = conn.prepareStatement("DELETE FROM USER WHERE nombre = ? and apellido = ?");
+			preparedStatement.setString(1, name);
+			preparedStatement.setString(2, surname);
+			preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		} finally {
+			if (null != conn) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+					throw new RuntimeException(e);
+				}
+			}
+			if (preparedStatement != null) {
+				try {
+					preparedStatement.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
 	}
 
 }
