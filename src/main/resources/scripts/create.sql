875c576fb0cb97a3fd4f82c3ee3cb4992c2c4138
JoTerrance <gorka.zgz@gmail.com>
2019-11-19 14:33:19 +0100

    fixed database sql

--- src/main/resources/scripts/create.sql
+++ src/main/resources/scripts/create.sql
@@ -1,12 +1,12 @@
+create table IF NOT EXISTS TEAM(
+    id identity primary key,
+    nombre varchar(25),
+    
+);
 create table IF NOT EXISTS USER(
     id identity primary key,
 	nombre varchar(25),
 	apellido varchar(25),
     codEquipo int,
     foreign key (codEquipo) references TEAM(id)
-);
-create table IF NOT EXISTS TEAM(
-    id identity primary key,
-    nombre varchar(25),
-    
 );
