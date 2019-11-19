b7a390db11f873f4c3e06f459e46d1b7ddb146df
JoTerrance <gorka.zgz@gmail.com>
2019-11-19 13:20:57 +0100

    Te arreglo los problemas del SQL pero ni uno mas, elresto toooooooooo pa ti
    si quieres volver a saber de mi, haz un pull request
    XOXOXO

--- src/main/resources/scripts/create.sql
+++ src/main/resources/scripts/create.sql
@@ -1,9 +1,9 @@
 create table IF NOT EXISTS USER(
     id identity primary key,
 	nombre varchar(25),
-	apellido varchar(25)
-    codEquipo integer,
-    codEquipo references EQUIPO(id)
+	apellido varchar(25),
+    codEquipo int,
+    foreign key (codEquipo) references TEAM(id)
 );
 create table IF NOT EXISTS TEAM(
     id identity primary key,
