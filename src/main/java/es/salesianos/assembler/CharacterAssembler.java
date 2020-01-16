e323120e93368c19118b80dcba0e35b314605d70
JuanPardos <juanpavh97@gmail.com>
2020-01-16 21:05:53 +0100

    Comprobación de portador único.
    Signed-off-by: JuanPardos <juanpavh97@gmail.com>

--- src/main/java/es/salesianos/assembler/CharacterAssembler.java
+++ src/main/java/es/salesianos/assembler/CharacterAssembler.java
@@ -1,13 +1,42 @@
 package es.salesianos.assembler;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import javax.servlet.http.HttpServletRequest;
 
 import es.salesianos.model.Character;
+import es.salesianos.repository.AbstractRepository;
+import es.salesianos.repository.CharacterRepository;
 
-public class CharacterAssembler {
+public class CharacterAssembler extends AbstractRepository {
+	private String portador;
+
+	public String checkPortador() {
+		Connection conn = manager.open(jdbcUrl);
+		PreparedStatement preparedStatement = null;
+		try {
+			preparedStatement = conn
+				.prepareStatement("SELECT p.ID, COUNT (p.ID) AS Cantidad FROM PERSONAJE p WHERE p.PORTADOR=TRUE");
+			preparedStatement.executeQuery();
+			portador = preparedStatement.toString();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		} finally {
+			manager.close(preparedStatement);
+			manager.close(conn);
+		}
+		return portador;
+	}
 
 	public Character createCharacterFromRequest(HttpServletRequest request) {
+		CharacterRepository charrepo = new CharacterRepository();
 		Character character = new Character();
+		if (portador == "1" && request.getParameter("holder") != "false")
+			throw new RuntimeException("Solo puede haber un portador");
 		character.setName(request.getParameter("name"));
 		character.setHolder(Boolean.parseBoolean(request.getParameter("holder")));
 		character.setCodRace(Long.parseLong(request.getParameter("codRace")));
