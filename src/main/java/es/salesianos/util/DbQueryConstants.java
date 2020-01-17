f595438263c1056878770f9c4afc873fed53ad92
Adrian Cachay <acachayep@gmail.com>
2020-01-17 16:42:07 +0100

    Hago el INNER JOIN
    Signed-off-by: Adrian Cachay <acachayep@gmail.com>

--- src/main/java/es/salesianos/util/DbQueryConstants.java
+++ src/main/java/es/salesianos/util/DbQueryConstants.java
@@ -2,10 +2,12 @@ package es.salesianos.util;
 
 public abstract class DbQueryConstants {
 	//SELECT
-	public static final String SELECT_ALL_CHARACTER = "SELECT * FROM personaje";
+	public static final String SELECT_ALL_CHARACTER = "SELECT * FROM personaje INNER JOIN raza "
+			+ "ON personaje.codRaza =  raza.id";
 	public static final String SELECT_ALL_RACE = "SELECT * FROM raza";
 	public static final String SELECT_CHARACTER_BY_ID = "SELECT * FROM personaje WHERE id=?";
 	public static final String SELECT_RACE_BY_ID = "SELECT * FROM raza WHERE id=?";
+
 	// INSERT
 	public static final String INSERT_CHARACTER = "INSERT INTO personaje (nombre,portador,codRaza)" + "VALUES (?, ?, ?)";
 	public static final String INSERT_RACE = "INSERT INTO raza (especie)" + " VALUES (?)";
