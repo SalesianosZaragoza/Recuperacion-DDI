7053f54eacc7119eb6af698e8573f525f47104ba
marcos <marcos.agni@gmail.com>
2021-01-07 09:03:54 +0100

    quito codigo comentado

--- Examen_B/src/main/java/es/salesianos/repository/BookRepository.java
+++ Examen_B/src/main/java/es/salesianos/repository/BookRepository.java
@@ -52,101 +52,6 @@ public class BookRepository extends Repository {
 			manager.close(conn);
 		}
 	}
-	/*
-	public void update(Book book) {
-		Connection conn = manager.open(jdbcUrl);
-		PreparedStatement preparedStatement = null;
-		try {
-			preparedStatement = conn.prepareStatement("UPDATE LIBRO SET isbn=?, fechaEdicion=?, almacen_old=?, almacen_new=? WHERE id=?");
-			preparedStatement.setString(1, book.getIsbn());
-			preparedStatement.setDate(2, new java.sql.Date(book.getEditionDate().getTime()));
-			if (book.isWarehouseOld()) {
-				preparedStatement.setInt(3, book.getWarehouseId());
-				preparedStatement.setNull(4, java.sql.Types.NULL); 
-			} else {
-				preparedStatement.setNull(3, java.sql.Types.NULL);
-				preparedStatement.setInt(4, book.getWarehouseId());
-			}
-			preparedStatement.setInt(5, book.getId());
-			preparedStatement.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		} finally {
-			close(preparedStatement);
-			manager.close(conn);
-		}
-	}
-	
-	public Warehouse getWarehouse(Book book) {
-		Integer id = (book.getAlmacen_old() > book.getAlmacen_new()) ? book.getAlmacen_old() : book.getAlmacen_new();
-		boolean isOld = book.getAlmacen_old().equals(id);
-		WarehouseRepository repository = new WarehouseRepository();
-		Warehouse warehouse = isOld ? repository.findByIdOld(id) : repository.findByIdNew(id);
-		return warehouse;
-	}
-	
-	public Book findById(Integer id) {
-		Book book = null;
-		Connection conn = manager.open(jdbcUrl);
-		ResultSet resultSet = null;
-		PreparedStatement prepareStatement = null;
-		try {
-			prepareStatement = conn.prepareStatement("SELECT * FROM LIBRO WHERE id=?");
-			prepareStatement.setInt(1, id);
-			resultSet = prepareStatement.executeQuery();
-			while (resultSet.next()) {
-				book = new Book(
-						resultSet.getInt(1),
-						resultSet.getString(2),
-						resultSet.getString(3),
-						resultSet.getInt(4),
-						resultSet.getInt(5)
-				);
-				book.setWarehouse(getWarehouse(book));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		} finally {
-			close(resultSet);
-			close(prepareStatement);
-			manager.close(conn);
-		}
-
-		return book;
-	}
-	
-	public List<Book> listAll() {
-		List<Book> bookList = new ArrayList<Book>();
-		Connection conn = manager.open(jdbcUrl);
-		ResultSet resultSet = null;
-		PreparedStatement prepareStatement = null;
-		try {
-			prepareStatement = conn.prepareStatement("SELECT * FROM LIBRO");
-			resultSet = prepareStatement.executeQuery();
-			while (resultSet.next()) {
-				Book book = new Book(
-						resultSet.getInt(1),
-						resultSet.getString(2),
-						resultSet.getString(3),
-						resultSet.getInt(4),
-						resultSet.getInt(5)
-					);
-				book.setWarehouse(getWarehouse(book));
-				bookList.add(book);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		} finally {
-			close(resultSet);
-			close(prepareStatement);
-			manager.close(conn);
-		}
-
-		return bookList;
-	} */
 	
 	private void close(PreparedStatement prepareStatement) {
 		if (null != prepareStatement) {
@@ -158,17 +63,4 @@ public class BookRepository extends Repository {
 			}
 		}
 	}
-	/*
-	private void close(ResultSet resultSet) {
-		if (null != resultSet) {
-
-			try {
-				resultSet.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-				throw new RuntimeException(e);
-			}
-		}
-	}
-	*/
 }
--- Examen_B/src/main/java/es/salesianos/service/BookService.java
+++ Examen_B/src/main/java/es/salesianos/service/BookService.java
@@ -20,29 +20,11 @@ public class BookService {
 		Book book = assembler.assembleFrom(req);
 		return book;
 	}
-	/*
-	public Book updateEntityFromRequest(HttpServletRequest req) {
-		Book book = assembler.assembleFrom(req);
-		return book;
-	}
-	*/
-
+	
 	public void insert(Book book) {
 		repository.insert(book);
 	}
-	/*
-	public void update(Book book) {
-		repository.update(book);
-	}
-	
-	public List<Book> listAll() {
-		return repository.listAll();
-	}
-	
-	public Book findById(Integer id) {
-		return repository.findById(id);
-	}
-	*/
+
 	public void delete(Book book) {
 		repository.delete(book);
 	}
